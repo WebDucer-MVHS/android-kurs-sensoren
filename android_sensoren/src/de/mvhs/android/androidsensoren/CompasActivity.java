4963efac852605bb83846f08a4446a152b3327c0
WebDucer <android@webducr.de>
2013-01-25 11:54:00 +0100

    Sensoren über richtige Hardware ansprechen

--- android_sensoren/src/de/mvhs/android/androidsensoren/CompasActivity.java
+++ android_sensoren/src/de/mvhs/android/androidsensoren/CompasActivity.java
@@ -1,12 +1,61 @@
 package de.mvhs.android.androidsensoren;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class CompasActivity extends Activity {
+public class CompasActivity extends Activity implements SensorEventListener {
+  private Sensor        _Sensor   = null;
+  private SensorManager _SManager = null;
+  private TextView      _XAxis    = null;
+  private TextView      _YAxis    = null;
+  private TextView      _ZAxis    = null;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_compas);
+
+    _SManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    _Sensor = _SManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+    _XAxis = (TextView) findViewById(R.id.txt_x_axis);
+    _YAxis = (TextView) findViewById(R.id.txt_y_axis);
+    _ZAxis = (TextView) findViewById(R.id.txt_z_axis);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (_Sensor != null) {
+      _SManager.registerListener(this, _Sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (_Sensor != null) {
+      _SManager.unregisterListener(this);
+    }
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    if (event.values.length >= 3) {
+      _XAxis.setText(String.valueOf(event.values[0]));
+      _YAxis.setText(String.valueOf(event.values[1]));
+      _ZAxis.setText(String.valueOf(event.values[2]));
+    }
   }
 }
--- android_sensoren/src/de/mvhs/android/androidsensoren/ProximityActivity.java
+++ android_sensoren/src/de/mvhs/android/androidsensoren/ProximityActivity.java
@@ -1,12 +1,55 @@
 package de.mvhs.android.androidsensoren;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ProximityActivity extends Activity {
+public class ProximityActivity extends Activity implements SensorEventListener {
+  private Sensor        _Sensor    = null;
+  private SensorManager _SManager  = null;
+  private TextView      _Proximity = null;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_proximity);
+
+    _SManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    _Sensor = _SManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    _Proximity = (TextView) findViewById(R.id.txt_priximity);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (_Sensor != null) {
+      _SManager.registerListener(this, _Sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (_Sensor != null) {
+      _SManager.unregisterListener(this);
+    }
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    if (event.values.length >= 1) {
+      _Proximity.setText(String.valueOf(event.values[0]));
+    }
   }
 }
